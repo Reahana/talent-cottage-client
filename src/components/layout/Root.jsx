a0606844dca9443c45bc4b05dd9c723dbdb355b9
Reahana <reahana233@gmail.com>
2024-06-02 11:45:28 +0600

    Main layout

--- src/components/layout/Root.jsx
+++ src/components/layout/Root.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Home from '../pages/Home/Home/Home';
+import { Outlet } from 'react-router-dom';
+import Header from '../pages/Shared/Header/Header';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
-            <h1>MAin </h1>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
