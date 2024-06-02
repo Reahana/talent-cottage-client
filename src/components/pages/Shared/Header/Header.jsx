1c722b9f234f86d3a706778190462495b727f04f
Reahana <reahana233@gmail.com>
2024-06-02 12:20:05 +0600

    navbar section

--- src/components/pages/Shared/Header/Header.jsx
+++ src/components/pages/Shared/Header/Header.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            header
+            <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+                <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt=""
+                    src="/img/logo.svg"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />{' '}
+                    React Bootstrap
+                </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Link>
+                            <Button variant='light'>Login</Button>
+                        </Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
