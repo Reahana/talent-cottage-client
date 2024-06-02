945497afaae47d343d423d91ee2288c06edf8481
Reahana <reahana233@gmail.com>
2024-06-02 12:19:56 +0600

    footer section

--- src/components/pages/Shared/Footer/Footer.jsx
+++ src/components/pages/Shared/Footer/Footer.jsx
@@ -6,17 +6,17 @@ const Footer = () => {
     return (
         <div className='bg-dark text-white py-3'>
             <Container fluid  >
-                <Row>
-                    <Col>
+                <Row >
+                    <Col sm={12} md={3}>
                         <h1>Talent Cottage</h1>
                     </Col>
-                    <Col>Site Map</Col>
-                    <Col>Get in Touch</Col>
-                    <Col>Visit us</Col>
+                    <Col  sm={12} md={3}>Site Map</Col>
+                    <Col  sm={12} md={3}>Get in Touch</Col>
+                    <Col  sm={12} md={3}>Visit us</Col>
                 </Row>
                 <hr />
                 <Row >
-                    <Col className='text-center'>
+                    <Col className='text-center ' sm={12}>
                         <p>Talent Cottage &copy; 
                             <Link to='https://reahanagomes.netlify.app' target='blank'>Amanda Reahana Gomes</Link>;
                         2024 || All Rights Reserved </p>
